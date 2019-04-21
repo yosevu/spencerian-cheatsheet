36b5ebd3100ddd1f31bed2bf153fc665b2780811
Yosevu Kilonzo <yosevuk@gmail.com>
2019-04-21 13:51:23 -0400

    Updates

---
+++ precache-manifest.e6ebf17b7a5ecc943462f5c1d6bac80b.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = [
+  {
+    "revision": "dada0a6947a0e53e718b555834b8c987",
+    "url": "/spencerian-cheatsheet/static/media/spencerian-guide-l.dada0a69.png"
+  },
+  {
+    "revision": "8e4ddb8e5d33b446efaf",
+    "url": "/spencerian-cheatsheet/static/css/main.890173fd.chunk.css"
+  },
+  {
+    "revision": "77a745546128b595c56f",
+    "url": "/spencerian-cheatsheet/static/js/runtime~main.1cd5d85d.js"
+  },
+  {
+    "revision": "e289355a03c004833b9e",
+    "url": "/spencerian-cheatsheet/static/js/2.eb2a2e41.chunk.js"
+  },
+  {
+    "revision": "84790937857c802b14d4a3c94354efae",
+    "url": "/spencerian-cheatsheet/static/media/spencerian-guide-a.84790937.png"
+  },
+  {
+    "revision": "3bc0607e82480e44629d2a5248e8a633",
+    "url": "/spencerian-cheatsheet/static/media/spencerian-guide-b.3bc0607e.png"
+  },
+  {
+    "revision": "10375b791c51ef2c18a82865c2093e04",
+    "url": "/spencerian-cheatsheet/static/media/spencerian-guide-c.10375b79.png"
+  },
+  {
+    "revision": "f97fc40ab73f81621651615e256379fb",
+    "url": "/spencerian-cheatsheet/static/media/spencerian-guide-d.f97fc40a.png"
+  },
+  {
+    "revision": "31831eeb5efc9fbd3fc56cfff2eaf397",
+    "url": "/spencerian-cheatsheet/static/media/spencerian-guide-e.31831eeb.png"
+  },
+  {
+    "revision": "535e03d670512951007b9cf0b441df28",
+    "url": "/spencerian-cheatsheet/static/media/spencerian-guide-f.535e03d6.png"
+  },
+  {
+    "revision": "05d0e5bfba8fae6cb85b3dd8dcedcac5",
+    "url": "/spencerian-cheatsheet/static/media/spencerian-guide-g.05d0e5bf.png"
+  },
+  {
+    "revision": "0399404a49498937fd1f93ee8bf37cd6",
+    "url": "/spencerian-cheatsheet/static/media/spencerian-guide-h.0399404a.png"
+  },
+  {
+    "revision": "15344841485914676a0112fa8b1f8364",
+    "url": "/spencerian-cheatsheet/static/media/spencerian-guide-i.15344841.png"
+  },
+  {
+    "revision": "67e841eacc50835e36e778b42992f644",
+    "url": "/spencerian-cheatsheet/static/media/spencerian-guide-j.67e841ea.png"
+  },
+  {
+    "revision": "da498068a8a8e15275ec3695741db974",
+    "url": "/spencerian-cheatsheet/static/media/spencerian-guide-k.da498068.png"
+  },
+  {
+    "revision": "8e4ddb8e5d33b446efaf",
+    "url": "/spencerian-cheatsheet/static/js/main.1f5065a9.chunk.js"
+  },
+  {
+    "revision": "adba05e48a71263a3ce0a27a56349995",
+    "url": "/spencerian-cheatsheet/static/media/spencerian-guide-m.adba05e4.png"
+  },
+  {
+    "revision": "68286bfa2d8a95d476c33f53a3e6c341",
+    "url": "/spencerian-cheatsheet/static/media/spencerian-guide-n.68286bfa.png"
+  },
+  {
+    "revision": "92a03ee6b1b474d6ba85406993e90576",
+    "url": "/spencerian-cheatsheet/static/media/spencerian-guide-o.92a03ee6.png"
+  },
+  {
+    "revision": "bc770882995c11ee9be3f607bba851dd",
+    "url": "/spencerian-cheatsheet/static/media/spencerian-guide-p.bc770882.png"
+  },
+  {
+    "revision": "fb6b504674e339571f48e2fdbed54ad6",
+    "url": "/spencerian-cheatsheet/static/media/spencerian-guide-q.fb6b5046.png"
+  },
+  {
+    "revision": "2de55a91e4f143a35f6c36e029ef309c",
+    "url": "/spencerian-cheatsheet/static/media/spencerian-guide-r.2de55a91.png"
+  },
+  {
+    "revision": "753b21d25662f08256fcc3100e84596e",
+    "url": "/spencerian-cheatsheet/static/media/spencerian-guide-s.753b21d2.png"
+  },
+  {
+    "revision": "928c3c25d92fdaf4004900422284c900",
+    "url": "/spencerian-cheatsheet/static/media/spencerian-guide-t.928c3c25.png"
+  },
+  {
+    "revision": "21e3eaf66d5c689bf1ac2c70c1086fe5",
+    "url": "/spencerian-cheatsheet/static/media/spencerian-guide-u.21e3eaf6.png"
+  },
+  {
+    "revision": "e13486f0c65fb9debcfb7fe8f2776b88",
+    "url": "/spencerian-cheatsheet/static/media/spencerian-guide-v.e13486f0.png"
+  },
+  {
+    "revision": "1f3646f8ac4638cac4233379e94af4c0",
+    "url": "/spencerian-cheatsheet/static/media/spencerian-guide-w.1f3646f8.png"
+  },
+  {
+    "revision": "38ecded4b6fda65b7c119d70143c8524",
+    "url": "/spencerian-cheatsheet/static/media/spencerian-guide-x.38ecded4.png"
+  },
+  {
+    "revision": "1f74956f318d69c7155ef5bfadf38dc9",
+    "url": "/spencerian-cheatsheet/static/media/spencerian-guide-y.1f74956f.png"
+  },
+  {
+    "revision": "b76cee4b447daf2853656428420574b2",
+    "url": "/spencerian-cheatsheet/static/media/spencerian-guide-z.b76cee4b.png"
+  },
+  {
+    "revision": "e4ba63d2d2bc2da6a2da5a073d095a1c",
+    "url": "/spencerian-cheatsheet/index.html"
+  }
+];
